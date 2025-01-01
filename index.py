777e269d495bf338fa28f306954a6689ef7e2ba7
Mihail-it <goncarovm4471@gmail.com>
2025-01-01 14:29:06 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-print('Hello world!')
+print('''
+      Привет! Это калькулятор процентов!
+      Ниже введи "Дано" свое задачи, где будет a/b/c
+      ''')
+print('Введи A')
+a = int(input())
+print('Введи B')
+b = int(input())
+print('Введи C')
+c = int(input())
+n = a + b + c
+print('Введи кол-во участников о которых тебе следует узнать.')
+t = int(input())
+x = n / t
+print('Ответ%:')
+print(x )
+
