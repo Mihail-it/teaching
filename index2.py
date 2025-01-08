6bbe3c7a3d0768fdb4d59e8cac65d783ea1d6d55
Mihail-it <goncarovm4471@gmail.com>
2025-01-08 18:22:18 +0300

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,11 @@
+txt1 = list('123')
+txt2 = list('456')
+
+txt1.extend(txt2)
+# .extend - Объединение списков
+print(txt1)
+
+txt3 = list('123')
+txt4 = list('456')
+print(txt3 + txt4)
+# Объединение списков с помошью "+"
